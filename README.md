da78532e81f894c077848e2be9b7a013c20129b8
fighting0701 <2551310133@qq.com>
2026-03-06 13:10:47 +0800

    第二步我们增加了一个功能！即输出你的输入

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # CLASS-HOMEWORK
 THIS IS FOR HOMEWORK
 THE first step --> output the sentence "Hello,World!"
+THE second step --> we can get the sentence we input.
--- homework.cpp
+++ homework.cpp
@@ -4,5 +4,9 @@ using namespace std;
 int main()
 {
     cout << "Hello,World!" << endl;
+    string s;
+    cout << "Please input :";
+    getline(cin,s);
+    cout << "Your input is :" << s << endl;
     return 0;
 }
