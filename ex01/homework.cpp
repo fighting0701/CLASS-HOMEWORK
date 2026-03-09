b540ba9ed5d4ddd5cd738b1d2d9f684f8fc592ab
fighting0701 <2551310133@qq.com>
2026-03-09 22:48:35 +0800

    第一步输出helloworld

--- ex01/homework.cpp
+++ ex01/homework.cpp
@@ -4,9 +4,5 @@ using namespace std;
 int main()
 {
     cout << "Hello,World!" << endl;
-    cout  << "Please input :";
-    string s;
-    getline(cin,s);
-    cout << "Your input is: " << s;
     return 0; 
 }
